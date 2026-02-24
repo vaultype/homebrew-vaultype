062ca873e2dd66da2e4e0e500e5f48480ebe9498
harungungorer <harungungorer@users.noreply.github.com>
2026-02-25 00:24:54 +0300

    chore: bump vaultype to v1.0

--- Casks/vaultype.rb
+++ Casks/vaultype.rb
@@ -1,6 +1,6 @@
 cask "vaultype" do
-  version "1.0.0-rc3"
-  sha256 "45762b95ef5c766b864d6992c3b301cd818890dc6d1b0bc8443df8d100a50d62"
+  version "1.0"
+  sha256 "f5bbc4f1479f7ac54b77d174f6ad7a61c6085f13c19237bddfab224df4b82edd"
 
   url "https://github.com/vaultype/VaulType/releases/download/v#{version}/VaulType-#{version}.dmg"
   name "VaulType"
@@ -8,12 +8,13 @@ cask "vaultype" do
   homepage "https://github.com/vaultype/VaulType"
 
   depends_on macos: ">= :sonoma"
+  depends_on arch: :arm64
 
   app "VaulType.app"
 
   zap trash: [
     "~/Library/Application Support/VaulType",
-    "~/Library/Preferences/com.vaultype.app.plist",
-    "~/Library/Caches/com.vaultype.app",
+    "~/Library/Preferences/com.vaultype.VaulType.plist",
+    "~/Library/Caches/com.vaultype.VaulType",
   ]
 end
